9d35114f9a22e37f91f62cff9ddcf41f2f0b7ab9
Aditya Dixit <dixitaditya20@gmail.com>
2021-05-14 13:18:24 +0530

    2nd Commit. JSP Page Added

--- Web-Application-using-Maven/src/main/java/webapp/LoginServlet.java
+++ Web-Application-using-Maven/src/main/java/webapp/LoginServlet.java
@@ -3,6 +3,7 @@ package webapp;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,18 +33,32 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Yahoo!!!!!!!!</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("My First Servlet");
-		out.println("</body>");
-		out.println("</html>");
+	//	@Override
+	//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	//		PrintWriter out = response.getWriter();
+	//		out.println("<html>");
+	//		out.println("<head>");
+	//		out.println("<title>Yahoo!!!!!!!!</title>");
+	//		out.println("</head>");
+	//		out.println("<body>");
+	//		out.println("My First Servlet");
+	//		out.println("</body>");
+	//		out.println("</html>");
+	//
+	//	}
 
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		/*Creating a Parameter Get Request
+		  Passing the Parameter from the Browser and Printing in Console Log*/
+		request.getParameter("name");
+
+		request.setAttribute("name", request.getParameter("name"));
+		request.setAttribute("password", request.getParameter("password"));
+
+		//Creating an connecting the JSP Page
+		request.getRequestDispatcher("/WEB-INF/Views/login.jsp").forward(request,response);
 	}
 
 }
